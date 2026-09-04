339c8b2afc3e4d67870c4a5fd3f0f4005ce3e88a
verishajir <verishajir@gmail.com>
2026-09-04 23:26:33 +0000

    Initial commit: tests/Engine.Tests/NetworkRegistryTests.cs

---
+++ tests/Engine.Tests/NetworkRegistryTests.cs
@@ -0,0 +1,24 @@
+using DcaBot.Engine.Networks;
+using Xunit;
+
+namespace DcaBot.Engine.Tests;
+
+public class NetworkRegistryTests
+{
+    [Fact]
+    public void GetNetwork_ReturnsEthereumDescriptor()
+    {
+        var registry = new NetworkRegistry();
+        var network = registry.GetNetwork("ethereum-mainnet");
+
+        Assert.Equal("ETH", network.NativeAssetSymbol);
+        Assert.Equal("evm", network.ChainType);
+    }
+
+    [Fact]
+    public void GetAllNetworks_ReturnsAtLeastFourNetworks()
+    {
+        var registry = new NetworkRegistry();
+        Assert.True(registry.GetAllNetworks().Count >= 4);
+    }
+}
